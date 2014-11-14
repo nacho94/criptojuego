1a7e4fec1e00440a27d7e453332772968f6e9671
Nacho Crespo <ignacio.crespo.martinez@gmail.com>
2014-11-14 18:39:19 +0100

    implementadas las correspondencias

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Main {
 
@@ -7,8 +8,10 @@ public class Main {
 	private static int numLetras = 0;
 	private static int numPalabras = 0;
 	private static ArrayList <String> palabras = new ArrayList <String>();
+	private static ArrayList <ArrayList <Integer>> palabrasDecodificadas = new ArrayList <ArrayList <Integer>>();
 	private static String clave = "";
 	private static ArrayList <String> diccionario = new ArrayList <String>();
+	private static HashMap <String, Integer> correspondencias = new HashMap < String,Integer>();
 
 	public static void main(String [] args) {
 
@@ -41,6 +44,13 @@ public class Main {
 		}
 
 		clave = scan.nextLine();
+		int indice = 1;
+		for(char c : clave.toCharArray()) {
+			if(anyadirCorrespondencias(c + "" ,indice)) {
+				indice++;
+			}
+
+		}
 
 		String linea = "";
 
@@ -48,9 +58,37 @@ public class Main {
 			linea = scan.nextLine();
 			if(!linea.equals("*")) {
 				diccionario.add(linea);
+				inicializarCorrespondencias(linea);
 			}
 
 		}while(!linea.equals("*"));
 
+	}
+
+	public static void inicializarCorrespondencias(String palabra) {
+		for(int i=0; i<palabra.length(); i++) {
+			correspondencias.put(palabra.charAt(i) + "",0);
+		}
+	}
+
+	public static void decodificarPalabras() {
+		for (int i=0; i<palabras.size(); i++) {
+			ArrayList <Integer> a = new ArrayList <Integer>();
+			for(String s : palabras.get(i).split(" ")) {
+				if(!s.equals("0")) {
+					a.add(Integer.parseInt(s));
+				}
+			}
+			palabrasDecodificadas.add(a);
+		}
+
+	}
+
+	public static boolean anyadirCorrespondencias(String letra ,Integer numero) {
+		if(correspondencias.get(letra)<=0) {
+			correspondencias.put(letra,numero);
+			return true;
+		}
+		return false;
 	}
 }
