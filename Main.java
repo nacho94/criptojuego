d9534a40e4514d9f4306733fc103446961d1dc42
Nacho Crespo <ignacio.crespo.martinez@gmail.com>
2014-11-14 17:44:54 +0100

    entrada implementada

--- Main.java
+++ Main.java
@@ -1,15 +1,56 @@
-
-
+import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 
-	private int numTest = 0;
-	private int numLetras = 0;
-	private int numPalabras = 0;
-	private ArrayList <String> palabras;
-	
+	private static int numTest = 0;
+	private static int numLetras = 0;
+	private static int numPalabras = 0;
+	private static ArrayList <String> palabras = new ArrayList <String>();
+	private static String clave = "";
+	private static ArrayList <String> diccionario = new ArrayList <String>();
+
 	public static void main(String [] args) {
 
+
+	}
+
+	public static void pedirEntrada () {
+
+		Scanner scan = new Scanner(System.in);
+
+		
+		numTest = scan.nextInt();
+
+		for(int i=0; i<numTest; i++) {
+			pedirTest(scan);
+		}
+
+	}
+
+	public static void pedirTest(Scanner scan) {
+		numLetras = scan.nextInt();
+		numPalabras = scan.nextInt();
+
+		palabras.clear();
+		diccionario.clear();
+
+		for (int i=0; i<numPalabras; i++) {
+			palabras.add(scan.nextLine());
+
+		}
+
+		clave = scan.nextLine();
+
+		String linea = "";
+
+		do{
+			linea = scan.nextLine();
+			if(!linea.equals("*")) {
+				diccionario.add(linea);
+			}
+
+		}while(!linea.equals("*"));
 
 	}
 }
