33761aa641948a201a4b04b79fd137271b187de7
Nacho Crespo <ignacio.crespo.martinez@gmail.com>
2014-11-14 17:12:04 +0100

    principio

---
+++ Main.java
@@ -0,0 +1,15 @@
+
+
+
+public class Main {
+
+	private int numTest = 0;
+	private int numLetras = 0;
+	private int numPalabras = 0;
+	private ArrayList <String> palabras;
+	
+	public static void main(String [] args) {
+
+
+	}
+}
